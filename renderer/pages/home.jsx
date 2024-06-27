71e8b184c5ac058cd6950b580df89694b5cc5996
Teuku Reynaldi <teukureynaldi106@gmail.com>
2024-06-27 23:30:28 +0700

    delete usestate state

--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 export default function HomePage() {
-  const [message, setMessage] = React.useState('No message found')
+  const [message, setMessage] = React.useState('')
 
   React.useEffect(() => {
     window.ipc.on('message', (message) => {
