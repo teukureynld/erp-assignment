830b40dba60d45063d0e57c72698ed4acd512b35
Teuku Reynaldi <teukureynaldi106@gmail.com>
2024-06-28 00:12:58 +0700

    login-page succesfuly done

--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -1,42 +1,31 @@
-import React from 'react'
-import Head from 'next/head'
-import Link from 'next/link'
-import Image from 'next/image'
+"use client";
+import React from 'react';
+import Head from 'next/head';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 export default function HomePage() {
-  const [message, setMessage] = React.useState('')
-
-  React.useEffect(() => {
-    window.ipc.on('message', (message) => {
-      setMessage(message)
-    })
-  }, [])
 
   return (
     <React.Fragment>
       <Head>
-        <title>Home - Nextron (basic-lang-javascript)</title>
+        <title>Karyawan Login</title>
       </Head>
-      <div>
-        <p>
-          ⚡ Electron + Next.js ⚡ - <Link href="/next">Go to next page</Link>
-        </p>
-        <Image
-          src="/images/logo.png"
-          alt="Logo image"
-          width={256}
-          height={256}
-        />
-      </div>
-      <div>
-        <button
-          onClick={() => {
-            window.ipc.send('message', 'Hello')
-          }}
-        >
-          Test IPC
-        </button>
-        <p>{message}</p>
+
+      <div class="container my-5">
+        <h1 style={{color : 'blue'}}>Login To Your Account</h1>
+        <form>
+          <div class="form-group">
+            <label for="input-username">username</label>
+            <input type="email" class="form-control" id="input-username" aria-describedby="emailHelp" placeholder=""></input>
+          </div>
+          <div class="form-group">
+            <label for="input-password">Password</label>
+            <input type="password" class="form-control" id="input-password" placeholder=""></input>
+          </div>
+          <div class="form-group form-check">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
       </div>
     </React.Fragment>
   )
