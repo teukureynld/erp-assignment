83f7c7de806703a27db83c8b762661c9aa5e621d
Teuku Reynaldi <teukureynaldi106@gmail.com>
2024-06-30 12:58:22 +0700

    (fix): migrating loginform code to its file and deleting form component

--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import WorkerForm from '../components/WorkerForm';
+import LoginForm from "../components/LoginForm";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -13,26 +14,6 @@ export default function HomePage() {
       <Head>
         <title>Karyawan Login</title>
       </Head>
-
-      <div className="container my-5">
-        <h1 style={{color : 'blue'}}>Login To Your Account</h1>
-        <form>
-          <div className="form-group">
-            <label htmlFor="input-username">Username</label>
-            <input type="email" className="form-control" id="input-username" aria-describedby="emailHelp" placeholder=""></input>
-          </div>
-          <div className="form-group">
-            <label htmlFor="input-password">Password</label>
-            <input type="password" className="form-control" id="input-password" placeholder=""></input>
-          </div>
-          <div className="form-group form-check">
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-
-        {/* Include WorkerForm component here */}
-        <WorkerForm />
-      </div>
     </React.Fragment>
   )
 }
